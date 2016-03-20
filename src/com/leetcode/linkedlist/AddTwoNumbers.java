be6778380f2254222a06dc0c4f7ff050543ca5a3
guangoon <guangoon@gmail.com>
2016-03-20 22:07:17 +0800

    add package of tree math linkedlist and array

--- src/com/leetcode/linkedlist/AddTwoNumbers.java
+++ src/com/leetcode/linkedlist/AddTwoNumbers.java
@@ -8,7 +8,7 @@ package com.leetcode.linkedlist;
  * Output: 7 -> 0 -> 8
 */
 public class AddTwoNumbers {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         if(l1 == null && l2 == null)
         	return null;
         ListNode dummy = new ListNode(0);
@@ -17,6 +17,7 @@ public class AddTwoNumbers {
         
         while(l1 != null && l2 != null){
         	int sum = l1.val + l2.val + carry;
+        	//System.out.println(sum);
         	current.next = new ListNode(sum % 10);
         	carry = sum / 10;
         	current = current.next;
@@ -24,16 +25,18 @@ public class AddTwoNumbers {
         	l2 = l2.next;
         }
         
-        if(l1  != null){
+        while(l1  != null){
         	int sum = l1.val  + carry;
+        	//System.out.println(sum);
         	current.next = new ListNode(sum % 10);
         	carry = sum / 10;
         	current = current.next;
         	l1 = l1.next;
         }
         
-        if(l2  != null){
+        while(l2  != null){
         	int sum = l2.val  + carry;
+        	//System.out.println(sum);
         	current.next = new ListNode(sum % 10);
         	carry = sum / 10;
         	current = current.next;
@@ -46,4 +49,27 @@ public class AddTwoNumbers {
         
         return dummy.next;
     }
+    
+    public static void main(String[] args) {
+		String sl1 = "9>1>6";
+		String sl2 = "0";
+		
+		String[] asl1 = sl1.split(">");
+		String[] asl2 = sl2.split(">");
+		ListNode dummy1 = new ListNode(0);
+		ListNode dummy2 = new ListNode(0);
+		ListNode c1 = dummy1;
+		ListNode c2 = dummy2;
+		for(String string : asl1){
+			c1.next = new ListNode(Integer.parseInt(string));
+			c1 = c1.next;
+		}
+		
+		for(String string : asl2){
+			c2.next = new ListNode(Integer.parseInt(string));
+			c2 = c2.next;
+		}
+		
+		addTwoNumbers(dummy1.next,dummy2.next);
+	}
 }
