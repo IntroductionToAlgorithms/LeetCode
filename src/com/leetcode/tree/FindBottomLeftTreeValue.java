3485fc30b0a50e62ba23adaed228c2f2a4a2c48f
Xiaowei Guan <guangoon@gmail.com>
2017-06-27 06:53:55 +0800

    add tree code

---
+++ src/com/leetcode/tree/FindBottomLeftTreeValue.java
@@ -0,0 +1,24 @@
+package com.leetcode.tree;
+
+/**
+ * Created by guangoon on 6/27/17.
+ */
+public class FindBottomLeftTreeValue {
+    private int mMaxL = -1;
+    private int mMaxLV = 0;
+    public int findBottomLeftValue(TreeNode root) {
+        dfs(root, 0);
+        return mMaxLV;
+    }
+
+    private void dfs(TreeNode x, int l){
+        if(x != null){
+            if(l > mMaxL) {
+                mMaxL = l;
+                mMaxLV = x.val;
+            }
+            dfs(x.left, l + 1);
+            dfs(x.right, l + 1);
+        }
+    }
+}
---
+++ src/com/leetcode/tree/FindLargestValueinEachTreeRow.java
@@ -0,0 +1,32 @@
+package com.leetcode.tree;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by guangoon on 6/27/17.
+ */
+public class FindLargestValueinEachTreeRow {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> res = new LinkedList<>();
+        if(root == null)
+            return res;
+        dfs(root, res, 0);
+        return res;
+    }
+
+    private void dfs(TreeNode x, List<Integer> res, int l){
+        if(x != null){
+            System.out.println(x.val + " : " + l + " : " + res.size());
+            if(l == res.size()){
+                res.add(x.val);
+            }else{
+                if(x.val > res.get(l)){
+                    res.set(l, x.val);
+                }
+            }
+            dfs(x.left, res, l + 1);
+            dfs(x.right, res, l + 1);
+        }
+    }
+}
